fedd10b689c0dbc9f8e5fed2bb5a0926eca95022
daniel rozenblat <danielroz12345@gmail.com>
2025-07-22 20:13:05 +0300

    newRecsss

--- src/thank/Thanks.js
+++ src/thank/Thanks.js
@@ -2,8 +2,16 @@ import Works from "../components/works/Works"
 import styles from "./Thanks.module.css"
   
 import Recommends from "../components/recommends/Works"
+import YouTubeShortsCarousel from "../components/shorts/Shorts";
 const Thanks=()=>{
-
+   const shortsIds = [
+        'bT61COiNKGM', // https://www.youtube.com/shorts/bT61COiNKGM
+        '31ehauP2cRE', // https://www.youtube.com/shorts/31ehauP2cRE
+        'A0E6G1MIWrU', // https://www.youtube.com/shorts/A0E6G1MIWrU
+        'xqOb0FGbeJo', // https://www.youtube.com/shorts/xqOb0FGbeJo
+        'udpA5LMr-Xc',
+        'iOev0zVeO0Y'
+      ];
 
 return <>
 <div style={{width:'100%', justifyContent:"center", overflow:"hidden" ,background:"linear-gradient(135deg, rgba(248,238,255,1) 15%, rgba(224,210,226,0.9915615904564951) 30%, rgba(248,238,255,1) 50%, rgba(224,210,226,1) 70%, rgba(248,238,255,1) 85%)"}}>
@@ -13,6 +21,8 @@ return <>
 <Works/>
 <div className={styles.subtitle}>החליקי לצדדים כדי לראות קצת ממה שהלקוחות שלי אמרו בסוף התהליך</div>
 <Recommends/>
+<div className={styles.subtitle}>חלקן רצו גם לדבר:</div>
+ <YouTubeShortsCarousel shortsIds={shortsIds}/>
 <div className={styles.subtitle}>כולי תקווה שאת/ה תהיה התמונה הבאה כאן</div>
 </div>
 </>
